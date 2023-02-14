a6a0484c78730f735337a903d16081c2c1b3ddc5
SrihariDUGGIRALA <srihari.duggirala@cyient.com>
2023-02-14 13:16:17 +0530

    Initial commit

---
+++ print.py
@@ -0,0 +1,5 @@
+Player = ['kohli','Dhoni','Raghul']
+message=" You are invited for dinner today"
+print(Player[0].title()+message)
+print(Player[1].title()+message)
+print(Player[2].title()+message)
